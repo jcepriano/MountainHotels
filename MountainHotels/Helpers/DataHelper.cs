28faca1a218c074322853074ee100c172fa9941d
jcepriano <jamescepriano@gmail.com>
2023-12-13 15:09:07 -0700

    deploy

---
+++ MountainHotels/Helpers/DataHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MountainHotels.DataAccess;
+
+namespace MountainHotels.Helpers
+{
+    public static class DataHelper
+    {
+
+        public static async Task ManageDataAsync(IServiceProvider svcProvider)
+        {
+            //Service: An instance of db context
+            var dbContextSvc = svcProvider.GetRequiredService<MountainHotelsContext>();
+
+            //Migration: This is the programmatic equivalent to Update-Database
+            await dbContextSvc.Database.MigrateAsync();
+        }
+
+
+    }
+}
